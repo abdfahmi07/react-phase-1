93107500c60ef7bdc6788a76b75f7e542dfc2df0
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-02-11 22:24:28 +0700

    conditional rendering with ternary operator

--- public/app.js
+++ public/app.js
@@ -2,16 +2,7 @@ const root = document.querySelector("#root");
 
 function App() {
   const [isLogin, setLogin] = React.useState(false);
-
-  if (isLogin) {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Sudah login, Bro!"), /*#__PURE__*/React.createElement("button", {
-      onClick: function () {
-        setLogin(false);
-      }
-    }, "Logout"));
-  }
-
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login dulu, Bro!"), /*#__PURE__*/React.createElement("button", {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login dulu, Bro!"), /*#__PURE__*/React.createElement("p", null, isLogin ? "Kamu sudah login" : "Kamu belum login"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
       setLogin(true);
     }
--- src/app.js
+++ src/app.js
@@ -3,24 +3,10 @@ const root = document.querySelector("#root");
 function App() {
   const [isLogin, setLogin] = React.useState(false);
 
-  if (isLogin) {
-    return (
-      <>
-        <h1>Sudah login, Bro!</h1>
-        <button
-          onClick={function () {
-            setLogin(false);
-          }}
-        >
-          Logout
-        </button>
-      </>
-    );
-  }
-
   return (
     <>
       <h1>Login dulu, Bro!</h1>
+      <p>{isLogin ? "Kamu sudah login" : "Kamu belum login"}</p>
       <button
         onClick={function () {
           setLogin(true);
