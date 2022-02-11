3be7638da83494dc428f3684b9f7bb75fada1c21
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-02-11 22:42:46 +0700

    use ref for manipulating dom

--- public/app.js
+++ public/app.js
@@ -2,11 +2,16 @@ const root = document.querySelector("#root");
 
 function App() {
   const [isLogin, setLogin] = React.useState(false);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login dulu, Bro!"), /*#__PURE__*/React.createElement("p", null, isLogin ? "Kamu sudah login" : "Kamu belum login"), /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setLogin(true);
-    }
-  }, "Login"));
+  const judulRef = React.useRef(null);
+  console.log("before", judulRef);
+  React.useEffect(function () {
+    setTimeout(function () {
+      judulRef.current.textContent = "Aplikasi";
+    }, 1000);
+  }, []);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    ref: judulRef
+  }, "Application"));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const root = document.querySelector("#root");
 
 function App() {
   const [isLogin, setLogin] = React.useState(false);
+  const judulRef = React.useRef(null);
+  console.log("before", judulRef);
+
+  React.useEffect(function () {
+    setTimeout(function () {
+      judulRef.current.textContent = "Aplikasi";
+    }, 1000);
+  }, []);
 
   return (
     <>
-      <h1>Login dulu, Bro!</h1>
-      <p>{isLogin ? "Kamu sudah login" : "Kamu belum login"}</p>
-      <button
-        onClick={function () {
-          setLogin(true);
-        }}
-      >
-        Login
-      </button>
+      <h1 ref={judulRef}>Application</h1>
     </>
   );
 }
